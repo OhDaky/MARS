3adfc68f8aa8f5c0b0a8bdea453305751102f612
sungyejin <yegene010@gmail.com>
2019-05-08 00:11:33 +0900

    first commit

--- unity_v_1/Assets/Join.cs
+++ unity_v_1/Assets/Join.cs
@@ -11,11 +11,17 @@ public class Join : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (Application.platform == RuntimePlatform.Android)
+		{
+			if (Input.GetKeyDown(KeyCode.Escape))
+			{
+				SceneManager.LoadScene(0);
+			}
+		}
+	}
 
 	public void Click()
 	{
--- unity_v_1/Assets/Login.cs
+++ unity_v_1/Assets/Login.cs
@@ -11,11 +11,17 @@ public class Login : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (Application.platform == RuntimePlatform.Android)
+		{
+			if (Input.GetKeyDown(KeyCode.Escape))
+			{
+				SceneManager.LoadScene(0);
+			}
+		}
+	}
 
 	public void Click()
 	{
